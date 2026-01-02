c3bf2bf897f23de938399a122238f565dd312eec
moon <megan.950628@gmail.com>
2026-01-02 15:29:04 +0800

    Add Dockerfile and deployment config

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Documents\B11317050\.venv
+set VIRTUAL_ENV=C:\Users\USER\B11317050\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
